c8cb57cc881e556c78177c53c81c576f2a098d6c
Leon Tan <leon.arian.tan@gmail.com>
2021-08-21 18:50:20 +0200

    Small fix such that example works

--- src/search/src/lib.rs
+++ src/search/src/lib.rs
@@ -1,6 +1,8 @@
+use ic_cdk::print;
 use ic_cdk_macros::query;
 
 #[query]
-fn print() {
-    ic_cdk::print("Hello World from DFINITY!");
+fn greet(name: String) -> String {
+    print("Hello World from DFINITY!");
+    format!("Hello {}", name)
 }
