2bac28b2d5f3253886d7a68c8dc0934c20e8a63a
nate <nathaniel.gurian@dfinity.org>
2021-08-26 09:16:27 -0400

    Integrated React as frontend Framework

--- src/search_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { search } from "../../declarations/search";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with search actor, calling the greet method
-  const greeting = await search.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
---
+++ src/search_assets/src/index.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { search } from "../../declarations/search";
+
+const App = () => {
+
+  return (
+    <div>
+      Dfinihack 2021 Champs
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("app"));
+
+
