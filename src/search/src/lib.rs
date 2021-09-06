ea6021e19a5946115195e142feb249eb646fd73a
Leon Tan <leon.tan@dfinity.org>
2021-09-06 15:47:15 +0200

    Add Environment Trait

--- src/search/src/lib.rs
+++ src/search/src/lib.rs
@@ -1,4 +1,4 @@
-use ic_cdk::caller;
+use ic_cdk::{api::time, caller, export::Principal};
 use ic_cdk_macros::query;
 
 #[query]
@@ -9,4 +9,41 @@ fn greet(name: String) -> String {
 #[query]
 fn greet_caller() -> String {
     format!("Hello {}", caller())
+}
+
+trait Environment {
+    fn get_caller(&self) -> Principal;
+    fn get_time(&self) -> u64;
+}
+
+struct CanisterEnvironment;
+
+impl Environment for CanisterEnvironment {
+    fn get_caller(&self) -> Principal {
+        caller()
+    }
+
+    fn get_time(&self) -> u64 {
+        time()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    struct TestEnvironment {
+        caller: Principal,
+        time: u64,
+    }
+
+    impl Environment for TestEnvironment {
+        fn get_caller(&self) -> Principal {
+            self.caller
+        }
+
+        fn get_time(&self) -> u64 {
+            self.time
+        }
+    }
 }
