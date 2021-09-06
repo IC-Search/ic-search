a0f95c657b5c34163953274bb8d614d2df356d11
Leon Tan <leon.tan@dfinity.org>
2021-09-06 15:41:32 +0200

    Add greet_caller function

--- src/search/src/lib.rs
+++ src/search/src/lib.rs
@@ -1,8 +1,12 @@
-use ic_cdk::print;
+use ic_cdk::caller;
 use ic_cdk_macros::query;
 
 #[query]
 fn greet(name: String) -> String {
-    print("Hello World from DFINITY!");
     format!("Hello {}", name)
+}
+
+#[query]
+fn greet_caller() -> String {
+    format!("Hello {}", caller())
 }
